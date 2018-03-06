af9c45d9c334c774b770c6b69c52f440ef96d98b
TurtleMan64 <turtleboy1026@aol.com>
2018-03-05 16:56:57 -0800

    Dynamically change skin with arrow keys

--- JoystickDisplay.cs
+++ JoystickDisplay.cs
@@ -29,33 +29,42 @@ namespace JoystickDisplay
 		
 		private static Pen penBlack;
 		
+		private static int folderIndex = 0;
+		
 		public Display()
 		{
 			joyX = 0;
 			joyY = 0;
-			A = 1;
-			B = 1;
-			X = 1;
-			Y = 1;
-			L = 1;
-			R = 1;
-			S = 1;
+			A    = 1;
+			B    = 1;
+			X    = 1;
+			Y    = 1;
+			L    = 1;
+			R    = 1;
+			S    = 1;
 			
-			imgBase = new Bitmap(@"res/base.png");
-			imgStick = new Bitmap(@"res/stick.png");
-			imgA = new Bitmap(@"res/buttA.png");
-			imgB = new Bitmap(@"res/buttB.png");
-			imgX = new Bitmap(@"res/buttX.png");
-			imgY = new Bitmap(@"res/buttY.png");
-			imgL = new Bitmap(@"res/buttL.png");
-			imgR = new Bitmap(@"res/buttR.png");
-			imgS = new Bitmap(@"res/buttS.png");
-			imgStickSmall = new Bitmap(@"res/stickSmall.png");
+			string path = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+			string[] folders = System.IO.Directory.GetDirectories(path,"*", System.IO.SearchOption.AllDirectories);
+			string folder = folders[0];
+			
+			imgBase       = new Bitmap(folder+"/base.png");
+			imgStick      = new Bitmap(folder+"/stick.png");
+			imgA          = new Bitmap(folder+"/buttA.png");
+			imgB          = new Bitmap(folder+"/buttB.png");
+			imgX          = new Bitmap(folder+"/buttX.png");
+			imgY          = new Bitmap(folder+"/buttY.png");
+			imgL          = new Bitmap(folder+"/buttL.png");
+			imgR          = new Bitmap(folder+"/buttR.png");
+			imgS          = new Bitmap(folder+"/buttS.png");
+			imgStickSmall = new Bitmap(folder+"/stickSmall.png");
 			
 			penBlack = new Pen(Color.Black, 1);
 			
 			this.DoubleBuffered = true;
-			this.Icon = new Icon("res/icon.ico");
+			this.Icon = new Icon("icon.ico");
+			
+			this.KeyPreview = true;
+			this.PreviewKeyDown += new PreviewKeyDownEventHandler(keyPress);
 		}
 		
 		public void setControllerDataSA2(int buttons, int newX, int newY)
@@ -184,58 +193,110 @@ namespace JoystickDisplay
 		
 		protected override void OnPaint(PaintEventArgs e)
 		{
-		   if (A != 0)
-		   {
-			   e.Graphics.DrawImage(imgA, 4, 100);
-		   }
+			if (A != 0)
+			{
+				e.Graphics.DrawImage(imgA, 4, 100);
+			}
 		   
-		   if (B != 0)
-		   {
-			   e.Graphics.DrawImage(imgB, 180, 100);
-		   }
+			if (B != 0)
+			{
+				e.Graphics.DrawImage(imgB, 180, 100);
+			}
 		   
-		   if (X != 0)
-		   {
+			if (X != 0)
+			{
 			   e.Graphics.DrawImage(imgX, 180, 52);
-		   }
+			}
 		   
-		   if (Y != 0)
-		   {
-			   e.Graphics.DrawImage(imgY, 4, 52);
-		   }
-		   
-		   if (L != 0)
-		   {
-			   e.Graphics.DrawImage(imgL, 4, 4);
-		   }
-		   
-		   if (R != 0)
-		   {
-			   e.Graphics.DrawImage(imgR, 180, 4);
-		   }
-		   
-		   if (S != 0)
-		   {
-			   e.Graphics.DrawImage(imgS, 156, 4);
-		   }
-		   
-		   int drawX = 108+((64*joyX)/128);
-		   int drawY = 68-((64*joyY)/128);
-		   
-		   e.Graphics.DrawImage(imgBase, 108-64, 68-64);
-		   e.Graphics.DrawLine(penBlack, 108, 68, drawX, drawY);
-		   e.Graphics.DrawImage(imgStickSmall, drawX-4, drawY-4);
-		   
-		   double radius = Math.Min(Math.Sqrt((joyX*joyX)+(joyY*joyY)), 128.0);
-		   double angle = Math.Atan2(joyY, joyX);
-		   int capX = (int)(radius*Math.Cos(angle));
-		   int capY = (int)(radius*Math.Sin(angle));
-		   int drawCapX = 108+((64*capX)/128);
-		   int drawCapY = 68-((64*capY)/128);
-		   
-		   e.Graphics.DrawImage(imgStick, drawCapX-4, drawCapY-4);
+			if (Y != 0)
+			{
+				e.Graphics.DrawImage(imgY, 4, 52);
+			}
 
-		   base.OnPaint(e);
+			if (L != 0)
+			{
+				e.Graphics.DrawImage(imgL, 4, 4);
+			}
+
+			if (R != 0)
+			{
+				e.Graphics.DrawImage(imgR, 180, 4);
+			}
+
+			if (S != 0)
+			{
+				e.Graphics.DrawImage(imgS, 156, 4);
+			}
+
+			int drawX = 108+((64*joyX)/128);
+			int drawY = 68-((64*joyY)/128);
+
+			e.Graphics.DrawImage(imgBase, 108-64, 68-64);
+			e.Graphics.DrawLine(penBlack, 108, 68, drawX, drawY);
+			e.Graphics.DrawImage(imgStickSmall, drawX-4, drawY-4);
+
+			double radius = Math.Min(Math.Sqrt((joyX*joyX)+(joyY*joyY)), 128.0);
+			double angle = Math.Atan2(joyY, joyX);
+			int capX = (int)(radius*Math.Cos(angle));
+			int capY = (int)(radius*Math.Sin(angle));
+			int drawCapX = 108+((64*capX)/128);
+			int drawCapY = 68-((64*capY)/128);
+
+			e.Graphics.DrawImage(imgStick, drawCapX-4, drawCapY-4);
+
+			base.OnPaint(e);
+		}
+		
+		public void keyPress(object sender, PreviewKeyDownEventArgs e)
+		{
+			string path = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+			string[] folders = System.IO.Directory.GetDirectories(path,"*", System.IO.SearchOption.AllDirectories);
+			
+			int prevFolderIndex = folderIndex;
+			
+			folderIndex = Math.Max(0, Math.Min(folders.Length-1, folderIndex));
+			
+			switch (e.KeyCode)
+			{
+				case Keys.Left:
+				{
+					folderIndex--;
+					if (folderIndex == -1)
+					{
+						folderIndex = folders.Length-1;
+					}
+					break;
+				}
+				
+				case Keys.Right:
+				{
+					folderIndex++;
+					if (folderIndex == folders.Length)
+					{
+						folderIndex = 0;
+					}
+					break;
+				}
+				
+				default:
+					break;
+			}
+			
+			if (folderIndex != prevFolderIndex)
+			{
+				string folder = folders[folderIndex];
+				
+				imgBase       = new Bitmap(folder+"/base.png");
+				imgStick      = new Bitmap(folder+"/stick.png");
+				imgA          = new Bitmap(folder+"/buttA.png");
+				imgB          = new Bitmap(folder+"/buttB.png");
+				imgX          = new Bitmap(folder+"/buttX.png");
+				imgY          = new Bitmap(folder+"/buttY.png");
+				imgL          = new Bitmap(folder+"/buttL.png");
+				imgR          = new Bitmap(folder+"/buttR.png");
+				imgS          = new Bitmap(folder+"/buttS.png");
+				imgStickSmall = new Bitmap(folder+"/stickSmall.png");
+			}
 		}
 	}
 }
