b398d0a3fc1e7f48420118743aefbf5508f005fc
TurtleMan64 <turtleboy1026@aol.com>
2018-08-18 23:46:34 -0700

    Button counting added

--- JoystickDisplay.cs
+++ JoystickDisplay.cs
@@ -16,6 +16,30 @@ namespace JoystickDisplay
 		private static int R;
 		private static int S;
 		
+		private static int prevA;
+		private static int prevB;
+		private static int prevX;
+		private static int prevY;
+		private static int prevL;
+		private static int prevR;
+		private static int prevS;
+		
+		private static int countA;
+		private static int countB;
+		private static int countX;
+		private static int countY;
+		private static int countL;
+		private static int countR;
+		private static int countS;
+		
+		private static Rectangle recA;
+		private static Rectangle recB;
+		private static Rectangle recX;
+		private static Rectangle recY;
+		private static Rectangle recL;
+		private static Rectangle recR;
+		private static Rectangle recS;
+		
 		private static Bitmap imgBase;
 		private static Bitmap imgStick;
 		private static Bitmap imgStickSmall;
@@ -28,8 +52,15 @@ namespace JoystickDisplay
 		private static Bitmap imgS;
 		
 		private static Pen penBlack;
+		private static Font fontArial;
+		
+		private static StringFormat formatLeft;
+		private static StringFormat formatCenter;
+		private static StringFormat formatRight;
 		
 		private static int folderIndex = 0;
+		
+		private static bool drawButtonCount = false;
 		
 		public Display()
 		{
@@ -42,6 +73,24 @@ namespace JoystickDisplay
 			L    = 1;
 			R    = 1;
 			S    = 1;
+			
+			prevA    = 1;
+			prevB    = 1;
+			prevX    = 1;
+			prevY    = 1;
+			prevL    = 1;
+			prevR    = 1;
+			prevS    = 1;
+			
+			countA = 0;
+			countB = 0;
+			countX = 0;
+			countY = 0;
+			countL = 0;
+			countR = 0;
+			countS = 0;
+			
+			
 			
 			try
 			{
@@ -67,6 +116,28 @@ namespace JoystickDisplay
 			imgStickSmall = new Bitmap(folder+"/stickSmall.png");
 			
 			penBlack = new Pen(Color.Black, 1);
+			fontArial = new Font("Arial", 16, FontStyle.Bold, GraphicsUnit.Point);
+			
+			recL = new Rectangle(40, 4  , 96, 36);
+			recY = new Rectangle(40, 52 , 96, 36);
+			recA = new Rectangle(40, 100, 96, 36);
+			recR = new Rectangle(80, 4  , 96, 36);
+			recX = new Rectangle(80, 52 , 96, 36);
+			recB = new Rectangle(80, 100, 96, 36);
+			recS = new Rectangle(60, 28 , 96, 36);
+			
+			formatLeft   = new StringFormat();
+			formatCenter = new StringFormat();
+			formatRight  = new StringFormat();
+			
+			formatLeft  .Alignment     = StringAlignment.Near;
+			formatLeft  .LineAlignment = StringAlignment.Center;
+			
+			formatCenter.Alignment     = StringAlignment.Center;
+			formatCenter.LineAlignment = StringAlignment.Center;
+			
+			formatRight .Alignment     = StringAlignment.Far;
+			formatRight .LineAlignment = StringAlignment.Center;
 			
 			this.DoubleBuffered = true;
 			this.Icon = new Icon("icon.ico");
@@ -111,6 +182,8 @@ namespace JoystickDisplay
 			{
 				joyY = -128;
 			}
+			
+			refreshButtonCounts();
 		}
 		
 		public void setControllerDataSADX(int buttons, int newX, int newY)
@@ -149,6 +222,8 @@ namespace JoystickDisplay
 			{
 				joyY = -128;
 			}
+			
+			refreshButtonCounts();
 		}
 		
 		public void setControllerDataHeroes(int buttons, float newX, float newY, float camPan)
@@ -197,6 +272,8 @@ namespace JoystickDisplay
 			{
 				joyY = -128;
 			}
+			
+			refreshButtonCounts();
 		}
 		
 		public void setControllerDataMania(int buttons, int stick)
@@ -205,7 +282,8 @@ namespace JoystickDisplay
 			B = buttons & 32;
 			Y = buttons & 128;
 			X = buttons & 64;
-			S = stick   & 32; //Enter key sets both 16 and 32 bit
+			S = stick   & 16;
+			
 			R = 0;
 			L = 0;
 			
@@ -234,6 +312,8 @@ namespace JoystickDisplay
 			{
 				joyY = -128;
 			}
+			
+			refreshButtonCounts();
 		}
 		
 		protected override void OnPaint(PaintEventArgs e)
@@ -268,28 +348,65 @@ namespace JoystickDisplay
 				e.Graphics.DrawImage(imgR, 180, 4);
 			}
 
-			if (S != 0)
+			if (!drawButtonCount)
 			{
-				e.Graphics.DrawImage(imgS, 156, 4);
+				if (S != 0)
+				{
+					e.Graphics.DrawImage(imgS, 156, 4);
+				}
+
+				int drawX = 108+((64*joyX)/128);
+				int drawY = 68 -((64*joyY)/128);
+
+				e.Graphics.DrawImage(imgBase, 108-64, 68-64);
+				e.Graphics.DrawLine(penBlack, 108, 68, drawX, drawY);
+				e.Graphics.DrawImage(imgStickSmall, drawX-4, drawY-4);
+
+				double radius = Math.Min(Math.Sqrt((joyX*joyX)+(joyY*joyY)), 128.0);
+				double angle  = Math.Atan2(joyY, joyX);
+				int capX = (int)(radius*Math.Cos(angle));
+				int capY = (int)(radius*Math.Sin(angle));
+				int drawCapX = 108+((64*capX)/128);
+				int drawCapY = 68 -((64*capY)/128);
+
+				e.Graphics.DrawImage(imgStick, drawCapX-4, drawCapY-4);
+			}
+			else
+			{
+				if (S != 0)
+				{
+					e.Graphics.DrawImage(imgS, 100, 12);
+				}
+				
+				e.Graphics.DrawString(""+countL, fontArial, Brushes.White, recL, formatLeft);
+				e.Graphics.DrawString(""+countY, fontArial, Brushes.White, recY, formatLeft);
+				e.Graphics.DrawString(""+countA, fontArial, Brushes.White, recA, formatLeft);
+				e.Graphics.DrawString(""+countR, fontArial, Brushes.White, recR, formatRight);
+				e.Graphics.DrawString(""+countX, fontArial, Brushes.White, recX, formatRight);
+				e.Graphics.DrawString(""+countB, fontArial, Brushes.White, recB, formatRight);
+				e.Graphics.DrawString(""+countS, fontArial, Brushes.White, recS, formatCenter);
 			}
 
-			int drawX = 108+((64*joyX)/128);
-			int drawY = 68-((64*joyY)/128);
-
-			e.Graphics.DrawImage(imgBase, 108-64, 68-64);
-			e.Graphics.DrawLine(penBlack, 108, 68, drawX, drawY);
-			e.Graphics.DrawImage(imgStickSmall, drawX-4, drawY-4);
-
-			double radius = Math.Min(Math.Sqrt((joyX*joyX)+(joyY*joyY)), 128.0);
-			double angle = Math.Atan2(joyY, joyX);
-			int capX = (int)(radius*Math.Cos(angle));
-			int capY = (int)(radius*Math.Sin(angle));
-			int drawCapX = 108+((64*capX)/128);
-			int drawCapY = 68-((64*capY)/128);
-
-			e.Graphics.DrawImage(imgStick, drawCapX-4, drawCapY-4);
-
 			base.OnPaint(e);
+		}
+		
+		public void refreshButtonCounts()
+		{
+			if (A != 0 && prevA == 0) { countA++; }
+			if (B != 0 && prevB == 0) { countB++; }
+			if (X != 0 && prevX == 0) { countX++; }
+			if (Y != 0 && prevY == 0) { countY++; }
+			if (L != 0 && prevL == 0) { countL++; }
+			if (R != 0 && prevR == 0) { countR++; }
+			if (S != 0 && prevS == 0) { countS++; }
+			
+			prevA = A;
+			prevB = B;
+			prevX = X;
+			prevY = Y;
+			prevL = L;
+			prevR = R;
+			prevS = S;
 		}
 		
 		public void keyPress(object sender, PreviewKeyDownEventArgs e)
@@ -320,6 +437,24 @@ namespace JoystickDisplay
 					{
 						folderIndex = 0;
 					}
+					break;
+				}
+				
+				case Keys.R:
+				{
+					countA = 0;
+					countB = 0;
+					countX = 0;
+					countY = 0;
+					countL = 0;
+					countR = 0;
+					countS = 0;
+					break;
+				}
+				
+				case Keys.B:
+				{
+					drawButtonCount = !drawButtonCount;
 					break;
 				}
 				
